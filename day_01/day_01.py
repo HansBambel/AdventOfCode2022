67d38a2bf3d564539d195562a97c6f280f0a1b5f
Kevin Trebing <kevin.trebing@plan-d.com>
2022-12-01 08:46:12 +0100

    Day 1

---
+++ day_01/day_01.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import numpy as np
+
+
+def calc_calories_and_elf(input_file: str):
+    data_ex = Path(__file__).with_name(input_file).read_text()
+
+    # Split into elves
+    elves_carrying = data_ex.split("\n\n")
+    # Split into calories per elf and convert to int
+    elves_calories = [calories.split("\n") for calories in elves_carrying]
+    elves_calories_int = [[int(cal) for cal in elf] for elf in elves_calories]
+
+    cal_sum = [sum(elf) for elf in elves_calories_int]
+    return cal_sum
+
+
+if __name__ == '__main__':
+    print("#" * 10 + " Part 1 " + "#" * 10)
+    cal_sum_ex = calc_calories_and_elf("input_ex.txt")
+    print(cal_sum_ex)
+    assert np.max(cal_sum_ex) == 24_000
+
+    cal_sum = calc_calories_and_elf("input.txt")
+    print(np.max(cal_sum))
+
+    # #### Part 2 ####
+    print("#" * 10 + " Part 2 " + "#" * 10)
+    assert sum(sorted(cal_sum_ex)[-3:]) == 45_000
+
+    print(sum(sorted(cal_sum)[-3:]))
--- template/day_xx.py
+++ template/day_xx.py
@@ -1,5 +1,32 @@
 from pathlib import Path
 
-data = Path(__file__).with_name("input_ex.txt").read_text()
 
-data = Path(__file__).with_name("input.txt").read_text()
+def part_1(input: str):
+    pass
+
+
+def part_2(input: str):
+    pass
+
+
+if __name__ == '__main__':
+    print("#"*10 + " Part 1 " + "#"*10)
+    data_ex = Path(__file__).with_name("input_ex.txt").read_text()
+    result_ex = part_1(data_ex)
+    print(result_ex)
+    assert result_ex == 1337
+
+    data = Path(__file__).with_name("input.txt").read_text()
+    result = part_1(data)
+    print(result)
+
+    # #### Part 2 ####
+    print("#"*10 + " Part 2 " + "#"*10)
+    data_ex = Path(__file__).with_name("input_ex.txt").read_text()
+    result = part_2(data_ex)
+    print(result)
+    assert result == 1337
+
+    data = Path(__file__).with_name("input.txt").read_text()
+    result = part_2(data)
+    print(result)
