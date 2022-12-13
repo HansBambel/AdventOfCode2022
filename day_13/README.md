01df05f83cc403400d6021ed4b74ef5e1641d33a
Kevin Trebing <kevin.trebing@plan-d.com>
2022-12-13 18:10:01 +0100

    day 13 part 2

--- day_13/README.md
+++ day_13/README.md
@@ -1,3 +1,7 @@
 # Part 1
 
 Not a fan of the wording of the task... Took me waaay too long to follow the instructions exactly.
+
+# Part 2
+
+Implementing BubbleSort was quite easy again.
--- day_13/day_13.py
+++ day_13/day_13.py
@@ -1,3 +1,4 @@
+from collections import deque
 from pathlib import Path
 from typing import Optional
 
@@ -45,7 +46,6 @@ def part_1(input_file: str):
     for i, pair in enumerate(pairs):
         left, right = pair.split("\n")
         correct_order = compare_left_right(eval(left), eval(right))
-        print(i, correct_order)
         if correct_order:
             sum_index += i + 1
 
@@ -54,7 +54,27 @@ def part_1(input_file: str):
 
 def part_2(input_file: str):
     data_file = Path(__file__).with_name(input_file).read_text()
-    input_data = data_file.split("\n")
+    lines = data_file.replace("\n\n", "\n").split("\n")
+    lines += ["[[2]]", "[[6]]"]
+
+    lines_evaluated = [eval(line) for line in lines]
+    # Write a sorting algorithm and reuse the compare function from earlier
+    sorted_list = deque([lines_evaluated.pop()])
+    for line in lines_evaluated:
+        for i, sorted_line in enumerate(sorted_list):
+            # True Left smaller
+            # False right smaller
+            # None both the same
+            cmp = compare_left_right(sorted_line, line)
+            if cmp is True:
+                continue
+            else:
+                sorted_list.insert(i, line)
+                break
+
+    # find the 2 and 6 again
+    indices = [i + 1 for i in range(len(sorted_list)) if sorted_list[i] in [[[2]], [[6]]]]
+    return indices[0] * indices[1]
 
 
 if __name__ == "__main__":
@@ -71,7 +91,7 @@ if __name__ == "__main__":
     print("#" * 10 + " Part 2 " + "#" * 10)
     result = part_2("input_ex.txt")
     print(result)
-    assert result == 1337
+    assert result == 140
 
     result = part_2("input.txt")
     print(result)
