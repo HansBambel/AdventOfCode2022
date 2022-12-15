336865b1197dcfb70f43405c601ebd6943477fca
Kevin Trebing <Kevin.Trebing@gmx.net>
2022-12-15 22:06:31 +0100

    day 15 part 2 optimization (now runs in a bit over a minute)

--- day_15/README.md
+++ day_15/README.md
@@ -6,3 +6,6 @@ Then, thinking about the problem I used.... MATH! Amazing thing, this math thing
 # Part 2
 **BRUTE FORCE**
 Took about 30min to solve. Checking for around 85mio values takes some time...
+
+## Speed-up
+After some speed-ups and some assumptions (that there is exactly one beacon) runtime goes down to a bit over a minute. Also because we got lucky with the position of the beacon.
--- day_15/day_15.py
+++ day_15/day_15.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 from pathlib import Path
 from typing import Tuple
 
@@ -68,58 +69,56 @@ def part_2(input_file: str, max_pos=20):
     data_file = Path(__file__).with_name(input_file).read_text()
     input_data = data_file.split("\n")
 
-    sensors = []
-    beacons = []
-    for line in input_data:
-        sensor, beacon = line.split(": ")
-        sensor: Tuple[int, int] = tuple(map(int, re.findall(r"x=(-?\d+), y=(-?\d+)", sensor)[0]))
-        beacon: Tuple[int, int] = tuple(map(int, re.findall(r"x=(-?\d+), y=(-?\d+)", beacon)[0]))
-        sensors.append(tuple(sensor))
-        beacons.append(tuple(beacon))
+    coords = [list(map(int, re.findall(r"(-?\d+)", line))) for line in input_data]
 
     """
     Idea: get the outer range+1 of the sensors outer range, remove those that are within the previous possible spaces
     """
     previous_ranges = []
-    possible_beacon_spots = set()
-    for sensor, beac in tqdm(zip(sensors, beacons), total=len(sensors), desc="Adding"):
+    possible_beacon_spots = Counter()
+    for sx, sy, bx, by in tqdm(coords, desc="Adding"):
         # Manhatten distance
+        sensor, beac = (sx, sy), (bx, by)
         dist_cur = l1_dist(sensor, beac)
 
-        rim_points = get_rim_edge(sensor, dist_cur)
-        possible_beacon_spots = possible_beacon_spots.union(rim_points)
+        rim_points = get_rim_edge(sensor, dist_cur, max_pos=max_pos)
+        # Add edges to counter
+        possible_beacon_spots.update(rim_points)
 
         previous_ranges.append((sensor, dist_cur))
 
-    # print("to check: ", len(possible_beacon_spots))
-    # remove those that are inside the previous sensors ranges
-    impossible = set()
-    for pos in tqdm(possible_beacon_spots, desc="Removing"):
-        for prev_sensor, dist in previous_ranges:
-            if l1_dist(prev_sensor, pos) <= dist:
-                impossible.add(pos)
-            if not (0 <= pos[0] <= max_pos) or not (0 <= pos[1] <= max_pos):
-                impossible.add(pos)
-    possible_beacon_spots = possible_beacon_spots - impossible
+    # Check these points that are inside the sensors ranges
+    # when there is one point that is not covered by sensors -> break
+    for pos, _ in tqdm(possible_beacon_spots.most_common(), desc="Checking"):
+        is_covered = False
+        for prev_sensor, sensor_range in previous_ranges:
+            if l1_dist(prev_sensor, pos) <= sensor_range:
+                is_covered = True
+                break
+        if not is_covered:
+            break
 
-    print(possible_beacon_spots)
-    assert len(possible_beacon_spots) == 1
-    beacon_spot = possible_beacon_spots.pop()
-    return beacon_spot[0] * 4_000_000 + beacon_spot[1]
+    print(pos)
+    return pos[0] * 4_000_000 + pos[1]
 
 
 def l1_dist(a: Tuple[int, int], b: Tuple[int, int]) -> int:
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 
-def get_rim_edge(pos: Tuple[int, int], dist: int) -> set[Tuple[int, int]]:
+def get_rim_edge(pos: Tuple[int, int], dist: int, max_pos: int) -> set[Tuple[int, int]]:
+    """Only add those that are still inside the field."""
     rim_points = set()
     for i in range(dist + 1):
         inverted = dist + 1 - i
-        rim_points.add((pos[0] - i, pos[1] + inverted))
-        rim_points.add((pos[0] - i, pos[1] - inverted))
-        rim_points.add((pos[0] + i, pos[1] + inverted))
-        rim_points.add((pos[0] + i, pos[1] - inverted))
+        for x, y in [
+            (pos[0] - i, pos[1] + inverted),
+            (pos[0] - i, pos[1] - inverted),
+            (pos[0] + i, pos[1] + inverted),
+            (pos[0] + i, pos[1] - inverted),
+        ]:
+            if (0 <= x < max_pos) and (0 <= y < max_pos):
+                rim_points.add((x, y))
 
     return rim_points
 
