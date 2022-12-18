23e7fbd00933b6fec266a5c459571f6cdec29b43
Kevin Trebing <Kevin.Trebing@gmx.net>
2022-12-18 13:53:59 +0100

    bullshit windows struggling to do basic shit

--- day_18/day_18.py
+++ day_18/day_18.py
@@ -1,4 +1,5 @@
 import re
+from collections import deque
 from pathlib import Path
 
 import numpy as np
@@ -7,14 +8,14 @@ import numpy as np
 def part_1(input_file: str):
     data_file = Path(__file__).with_name(input_file).read_text()
     input_data = data_file.split("\n")
-    droplets = [list(map(int, re.findall(r"(\d+)", coords))) for coords in input_data]
+    droplets = {tuple(map(int, re.findall(r"(\d+)", coords))) for coords in input_data}
 
     free_sides = 0
     for drop in droplets:
         # check neighbors
         free_sides += 6
         for neighbor in [[+1, 0, 0], [-1, 0, 0], [0, +1, 0], [0, -1, 0], [0, 0, +1], [0, 0, -1]]:
-            if list(np.array(drop) - np.array(neighbor)) in droplets:
+            if tuple(np.array(drop) - np.array(neighbor)) in droplets:
                 free_sides -= 1
     return free_sides
 
@@ -22,15 +23,36 @@ def part_1(input_file: str):
 def part_2(input_file: str):
     data_file = Path(__file__).with_name(input_file).read_text()
     input_data = data_file.split("\n")
-    droplets = [list(map(int, re.findall(r"(\d+)", coords))) for coords in input_data]
+    droplets = [tuple(map(int, re.findall(r"(\d+)", coords))) for coords in input_data]
 
-    droplets_array = np.array(droplets)
-    area = 0
-    # Idea: flooding algorithm
-    for z in range(np.max(droplets_array, axis=2), np.min(droplets_array, axis=2), 1):
-        pass
+    # Idea: Find the outside of the sphere
+    Q = deque([(0, 0, 0)])
+    rel_drops = set()
+    seen = set()
+    while len(Q) > 0:
+        drop = Q.popleft()
+        print(len(Q), len(rel_drops))
+        seen.add(drop)
 
-    return area
+        for neighbor in [[+1, 0, 0], [-1, 0, 0], [0, +1, 0], [0, -1, 0], [0, 0, +1], [0, 0, -1]]:
+            new_pixel = (drop[0] + neighbor[0], drop[1] + neighbor[1], drop[2] + neighbor[2])
+            if new_pixel in droplets:
+                rel_drops.add(drop)
+            # if outside of field -> skip
+            if any([c < 0 or c > 25 for c in new_pixel]) or new_pixel in seen or new_pixel in droplets:
+                continue
+            Q.append(new_pixel)
+
+    print("Done flooding")
+    print(rel_drops)
+    free_sides = 0
+    for drop in rel_drops:
+        # check neighbors
+        free_sides += 6
+        for neighbor in [[+1, 0, 0], [-1, 0, 0], [0, +1, 0], [0, -1, 0], [0, 0, +1], [0, 0, -1]]:
+            if tuple(np.array(drop) - np.array(neighbor)) in rel_drops:
+                free_sides -= 1
+    return free_sides
 
 
 if __name__ == "__main__":
