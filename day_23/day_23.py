d3f6b83a744fe02c4237bed652709741a4e71766
Kevin Trebing <kevin.trebing@plan-d.com>
2022-12-23 15:51:36 +0100

    day 23 part 2

--- day_23/day_23.py
+++ day_23/day_23.py
@@ -72,6 +72,53 @@ def part_1(input_file: str):
 def part_2(input_file: str):
     data_file = Path(__file__).with_name(input_file).read_text()
     input_data = data_file.split("\n")
+    north = [(-1, 0), (-1, 1), (-1, -1)]
+    south = [(+1, 0), (+1, 1), (+1, -1)]
+    west = [(0, -1), (-1, -1), (+1, -1)]
+    east = [(0, +1), (-1, +1), (+1, +1)]
+    directions = [north, south, west, east]
+    elves = set()
+    for row, line in enumerate(input_data):
+        for column, char in enumerate(line):
+            if char == "#":
+                elves.add((row, column))
+
+    moved = True
+    round = 0
+    while moved:
+        first_dir = round % len(directions)
+        wants_to_move = {}
+        # first half: check for possible movement
+        for elf_pos in elves:
+            # N,S,W,E
+            has_neighbor = False
+            for coarse_dir_i in range(len(directions)):
+                coarse_dir = directions[(first_dir + coarse_dir_i) % len(directions)]
+                has_elf = [(elf_pos[0] + direction[0], elf_pos[1] + direction[1]) in elves for direction in coarse_dir]
+                if any(has_elf):
+                    has_neighbor = True
+                    continue
+                else:
+                    if wants_to_move.get(elf_pos, None) is None:
+                        wants_to_move[elf_pos] = (elf_pos[0] + coarse_dir[0][0], elf_pos[1] + coarse_dir[0][1])
+            if not has_neighbor:
+                wants_to_move.pop(elf_pos)
+
+        # second half: do the move IF NO OTHER elf want to move there
+        clean_moves = {}
+        for elf_pos, new_pos in wants_to_move.items():
+            if new_pos in clean_moves.values():
+                # inefficient... remove the move that makes elves land on the same spot
+                clean_moves = {k: v for k, v in clean_moves.items() if v != new_pos}
+            else:
+                clean_moves[elf_pos] = new_pos
+        for elf_pos, new_pos in clean_moves.items():
+            elves.remove(elf_pos)
+            elves.add(new_pos)
+        round += 1
+        moved = len(clean_moves) > 0
+
+    return round
 
 
 if __name__ == "__main__":
